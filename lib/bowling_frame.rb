fd6be163ce1cd220272b5fef6650119d145d3633
nayan0499 <nayangrg.uni@gmail.com>
2023-09-07 18:39:48 +0100

    Add methods and tests

--- lib/bowling_frame.rb
+++ lib/bowling_frame.rb
@@ -1,11 +1,44 @@
 
 class BowlingFrame
-  attr_reader :first_roll_pins_knocked_down, :second_roll_pins_knocked_down, :total_pins_knocked_down
-    def initialize(first_roll_pins_knocked_down, second_roll_pins_knocked_down=0) 
+attr_reader   :first_roll_pins_knocked_down, :second_roll_pins_knocked_down, :total_pins_knocked_down, :next_frame
+    def initialize(first_roll_pins_knocked_down, second_roll_pins_knocked_down=0, next_frame = nil) 
       raise ArgumentError,"Invalid number of pins knocked down" if second_roll_pins_knocked_down == 0 && first_roll_pins_knocked_down != 10
         @first_roll_pins_knocked_down = first_roll_pins_knocked_down
         @second_roll_pins_knocked_down = second_roll_pins_knocked_down
         @total_pins_knocked_down = first_roll_pins_knocked_down + second_roll_pins_knocked_down
+        @next_frame = next_frame
         
     end 
-  end 
+
+    def get_bonus_roll_count
+      if @first_roll_pins_knocked_down == 10 
+        return 2
+      elsif @total_pins_knocked_down == 10
+        return 1
+      end 
+      0
+    end
+
+    def get_bonus_score
+      if get_bonus_roll_count == 1
+        return next_frame.first_roll_pins_knocked_down
+      elsif get_bonus_roll_count == 2
+        return next_frame.total_pins_knocked_down
+      end
+      0
+    end
+    def get_total_score
+      get_bonus_score + total_pins_knocked_down
+    end
+
+   
+
+    def ==(other)
+      return false unless other.is_a?(BowlingFrame)
+       @first_roll_pins_knocked_down = other.first_roll_pins_knocked_down && @second_roll_pins_knocked_down == other.second_roll_pins_knocked_down
+
+    end 
+
+
+
+end
--- rspec/frame_spec.rb
+++ rspec/frame_spec.rb
@@ -2,7 +2,7 @@
 require 'rspec'
 require 'bowling_frame'
 
-RSpec.describe "#initialize method" do 
+RSpec.describe "#initialize " do 
     it "get correct initialized parameters when getters are called" do 
         frame = BowlingFrame.new(3,5)
         expect(frame.first_roll_pins_knocked_down).to be(3)
@@ -11,7 +11,7 @@ RSpec.describe "#initialize method" do
     end 
 end
 
-RSpec.describe "#initialize method" do 
+RSpec.describe "#initialize " do 
     it "set second_roll_pins_knocked_down to 0 by default when no argument is passed" do 
         frame = BowlingFrame.new(10)
         expect(frame.first_roll_pins_knocked_down).to be(10)
@@ -20,11 +20,138 @@ RSpec.describe "#initialize method" do
     end 
 end
 
-RSpec.describe "#initialize method" do 
+RSpec.describe "#initialize " do 
     it "raises an error when the first_roll_pins_knocked_down is not 10 and the second argument is not provided" do 
      
         expect { BowlingFrame.new(6)
         }.to raise_error(ArgumentError, "Invalid number of pins knocked down")
+         
+    end 
+end
+
+RSpec.describe "#initialize" do 
+    it "returns the next frame when called" do 
+        next_frame = BowlingFrame.new(5,6)
+        frame = BowlingFrame.new(10,second_roll_pins_knocked_down=0, next_frame=next_frame)
+        next_frame = frame.next_frame
+        expect(next_frame).to eq(BowlingFrame.new(5,6))
+        
+         
+    end 
+end
+
+
+RSpec.describe "#get_bonus_roll_count " do 
+    it "get 0 when it is neither a strike nor a spare" do 
+     
+        frame = BowlingFrame.new(2,3)
+        bonus_roll_count = frame.get_bonus_roll_count 
+        expect(bonus_roll_count).to eq(0)
+        
+         
+    end 
+end
+
+RSpec.describe "#get_bonus_roll_count " do 
+    it "get 1 when it is a spare" do 
+     
+        frame = BowlingFrame.new(2,8)
+        bonus_roll_count = frame.get_bonus_roll_count 
+        expect(bonus_roll_count).to eq(1)
+        
+         
+    end 
+end
+
+RSpec.describe "#get_bonus_roll_count" do 
+    it "returns 2 when it is a strike" do 
+     
+        frame = BowlingFrame.new(10)
+        bonus_roll_count = frame.get_bonus_roll_count 
+        expect(bonus_roll_count).to eq(2)
+        
+         
+    end 
+end
+
+RSpec.describe "#get_bonus_score" do 
+    it "returns 0 when it is neither a strike nor a spare" do 
+     
+        frame = BowlingFrame.new(2,3)
+        bonus_score= frame.get_bonus_score
+        expect(bonus_score).to eq(0)
+        
+         
+    end 
+end
+
+
+RSpec.describe "#==" do 
+    it "returns true when all the instance variables are the same" do 
+        frame1 = BowlingFrame.new(10)
+        frame2 = BowlingFrame.new(10)
+        expect(frame1).to eq(frame2)
+        
+         
+    end 
+end
+
+RSpec.describe "#get_bonus_score" do 
+    it "returns next frame's first roll knocked down pins when it is a spare" do 
+        next_frame = BowlingFrame.new(5,6)
+        current_frame = BowlingFrame.new(8,second_roll_pins_knocked_down=2, next_frame=next_frame)
+        bonus_score= current_frame.get_bonus_score
+        expect(bonus_score).to eq(5)
+         
+    end 
+end
+
+RSpec.describe "#get_bonus_score" do 
+    it "returns next frame's total pins knocked down pins when it is a strike" do 
+        next_frame = BowlingFrame.new(5,6)
+        current_frame = BowlingFrame.new(10, second_roll_pins_knocked_down= 0,next_frame=next_frame)
+        bonus_score= current_frame.get_bonus_score
+        expect(bonus_score).to eq(11)
+         
+    end 
+end
+
+RSpec.describe "#get_bonus_score" do 
+    it "returns 10 + next next frame's first roll pins knocked down when the current frame and the next frame are strikes " do 
+        next_frame = BowlingFrame.new(5,6)
+        current_frame = BowlingFrame.new(10, second_roll_pins_knocked_down= 0,next_frame=next_frame)
+        bonus_score= current_frame.get_bonus_score
+        expect(bonus_score).to eq(11)
+         
+    end 
+end
+
+RSpec.describe "#get_total_score" do 
+    it "returns the sum of total pins knocked down and bonus when strike " do 
+        next_frame = BowlingFrame.new(5,6)
+        current_frame = BowlingFrame.new(10, second_roll_pins_knocked_down= 0,next_frame=next_frame)
+        bonus_score= current_frame.get_total_score
+        expect(bonus_score).to eq(21)
+         
+    end 
+end
+
+RSpec.describe "#get_total_score" do 
+    it "returns the sum of total pins knocked down and bonus when spare" do 
+        next_frame = BowlingFrame.new(5,6)
+        current_frame = BowlingFrame.new(2, second_roll_pins_knocked_down= 8,next_frame=next_frame)
+        bonus_score= current_frame.get_total_score
+        expect(bonus_score).to eq(15)
+         
+    end 
+end
+
+RSpec.describe "#get_total_score" do 
+    it "returns the sum of total pins knocked down and bonus when neither spare nor strike" do 
+        next_frame = BowlingFrame.new(5,6)
+        current_frame = BowlingFrame.new(2, second_roll_pins_knocked_down= 5,next_frame=next_frame)
+        bonus_score= current_frame.get_total_score
+        expect(bonus_score).to eq(7)
          
     end 
 end
